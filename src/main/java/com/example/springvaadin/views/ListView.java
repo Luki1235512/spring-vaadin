a7d179171d53d8a5665df5dfa12c9a64b0f6c76e
Luki1235512 <Luki1235512@gmail.com>
2022-04-12 17:14:37 +0200

    fixed import

--- src/main/java/com/example/springvaadin/views/ListView.java
+++ src/main/java/com/example/springvaadin/views/ListView.java
@@ -3,17 +3,16 @@ package com.example.springvaadin.views;
 import com.example.springvaadin.data.entity.Contact;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-
-import java.awt.*;
 
 @PageTitle("Contacts")
 @Route(value = "")
 public class ListView extends VerticalLayout {
 
     Grid<Contact> grid = new Grid<>(Contact.class);
-//    TextField filterText = new TextField();
+    TextField filterText = new TextField();
 
     public ListView() {
         addClassName("list-view");
