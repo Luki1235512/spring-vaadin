5ea958ab21cd6c1abc82e09e37b51bb6843d80d2
Luki1235512 <Luki1235512@gmail.com>
2022-04-12 16:32:31 +0200

    initial commit

---
+++ src/main/java/com/example/springvaadin/views/ListView.java
@@ -0,0 +1,17 @@
+package com.example.springvaadin.views;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Contacts")
+@Route(value = "")
+public class ListView extends VerticalLayout {
+
+    public ListView() {
+
+    }
+
+}
