0aa511e5a64be353ad767d9dd4039c384a892b50
Luki1235512 <Luki1235512@gmail.com>
2022-04-14 14:52:54 +0200

    added toolbar

--- src/main/java/com/example/springvaadin/data/entity/Contact.java
+++ src/main/java/com/example/springvaadin/data/entity/Contact.java
@@ -32,5 +32,48 @@ public class Contact extends AbstractEntity {
     @NotEmpty
     private  String email;
 
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/example/springvaadin/views/ListView.java
+++ src/main/java/com/example/springvaadin/views/ListView.java
@@ -1,9 +1,13 @@
 package com.example.springvaadin.views;
 
 import com.example.springvaadin.data.entity.Contact;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -19,11 +23,30 @@ public class ListView extends VerticalLayout {
         setSizeFull();
 
         configureGrid();
+
+        add(getToolbar(), grid);
+    }
+
+    private Component getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button addContactButton = new Button("Add contact");
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
     }
 
     private void configureGrid() {
         grid.addClassName("contact-grid");
         grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
     }
 
 }
